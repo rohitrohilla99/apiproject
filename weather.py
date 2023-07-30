45da093a739184c2777116b9553b7f22d00dad1b
rohitrohilla99 <101062985+rohitrohilla99@users.noreply.github.com>
2023-07-30 14:24:21 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,11 @@
+import requests,json
+apikey="07d7fbb973b1b48f10b7ff42dc3d30bc"
+city_name=input("Enter the city name:")
+temperature_data=requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&APPID={apikey}")
+b=temperature_data.json()["main"]["temp"]
+temp=b-273.15
+if(temperature_data.json()['cod']=='404'):
+    print("invalid data")
+else:
+    print(f"the temperature in {city_name} is:{temp}°C")
+
