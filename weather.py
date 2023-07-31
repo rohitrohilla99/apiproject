7c0aa66e800d038a3c1573fc76f0a3494564e70f
rohitrohilla99 <101062985+rohitrohilla99@users.noreply.github.com>
2023-07-31 16:38:30 +0530

    Add files via upload
    round of temperature

--- weather.py
+++ weather.py
@@ -4,6 +4,7 @@ city_name=input("Enter the city name:")
 temperature_data=requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&APPID={apikey}")
 b=temperature_data.json()["main"]["temp"]
 temp=b-273.15
+temp = round(temp,2);
 if(temperature_data.json()['cod']=='404'):
     print("invalid data")
 else:
